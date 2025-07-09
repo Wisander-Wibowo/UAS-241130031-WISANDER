33ef2f6d08ce3ef9068bc625fe6dca821ed643ab
Wisander-Wibowo <wisander.mainblog@gmail.com>
2025-07-09 19:07:45 +0700

    Tombol Plus

--- script/script.js
+++ script/script.js
@@ -11,4 +11,13 @@ document.querySelectorAll('.card-produk').forEach(card => {
         if(value > 1) {input.value = value - 1};
         harga.innerHTML = `$${(nominalHarga / value) * input.value}`
     })
+
+    plus.addEventListener('click', () => {
+        const harga = card.querySelector('.price');
+        const nominalHarga = harga.textContent.match(/\d+/)[0];
+
+        let value = parseInt(input.value);
+        input.value = value + 1;
+        harga.innerHTML = `$${(nominalHarga / value) * input.value}`
+    })
 })
