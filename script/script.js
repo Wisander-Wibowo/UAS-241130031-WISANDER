d0d6a86c54c4b60b1a0c3a7337bee20502d4d4ad
Wisander-Wibowo <wisander.mainblog@gmail.com>
2025-07-09 19:02:45 +0700

    Tombol Min

---
+++ script/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.card-produk').forEach(card => {
+    const input = card.querySelector('.quantity input');
+    const minus = card.querySelector('.minus')
+    const plus = card.querySelector('.plus')
+
+    minus.addEventListener('click', () => {
+        const harga = card.querySelector('.price');
+        const nominalHarga = harga.textContent.match(/\d+/)[0];
+
+        let value = parseInt(input.value);
+        if(value > 1) {input.value = value - 1};
+        harga.innerHTML = `$${(nominalHarga / value) * input.value}`
+    })
+})
